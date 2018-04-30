8a47f0d169b1f3a65f06dd7b96b00086d4e36548
Ross Stanton <ross_stanton@hotmail.com>
2018-04-30 17:48:33 +0100

    First commit

---
+++ site/src/main/java/com/stanton/wcms/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.stanton.wcms.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="atLaw:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
